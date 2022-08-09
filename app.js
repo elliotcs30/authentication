c69add7acbfd50e9ed586e6513787cc1acc054d0
Elliot <elliotcs30@gmail.com>
2022-08-09 19:17:33 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+
 const app = express()
 const PORT = 3000
 
-const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
 // 取得資料庫連線狀態
---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const usersSchema = new Schema({
+  firstName : {
+    type: 'string', // 資料型別是字串
+    require: true // 這是個必填欄位
+  },
+  email : {
+    type: 'string', // 資料型別是字串
+    require: true // 這是個必填欄位
+  },
+  password : {
+    type: 'string', // 資料型別是字串
+    require: true // 這是個必填欄位
+  },
+})
+
+module.exports = mongoose.model('Users', usersSchema)
